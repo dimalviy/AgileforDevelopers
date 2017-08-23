8cde4ce5e0ba9cffdce3d76f1eb66640b9e4ca95
dimalviy <dimalviy@DIN80011952.corp.capgemini.com>
2017-08-23 10:41:41 +0530

    hiii

---
+++ Day2-JavaApp/src/main/java/org/cap/boot/MianClass.java
@@ -0,0 +1,13 @@
+package org.cap.boot;
+
+public class MianClass {
+
+	public static void main(string[] args)
+	{
+		 display();
+		 salesclass obj = new salesclass();
+		 obj.show();
+		 obj.greetings();
+		
+	}
+}
